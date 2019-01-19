a1b8d8fd0d077410cc8928f0b00cbd159c0a7bdd
Edgar Meruvia <emeruvia@gmail.com>
2019-01-19 17:40:54 -0500

    Add skeleton for app

--- android-app/app/src/main/java/com/swamphacks/MainActivity.kt
+++ android-app/app/src/main/java/com/swamphacks/MainActivity.kt
@@ -2,11 +2,22 @@ package com.swamphacks
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  lateinit var image: ImageView
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    image = findViewById(R.id.main_iv)
+    image.setImageResource(R.drawable.receipt)
+  }
+
+  fun recognizeBtn(view: View) {
+    Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+  }
 }
