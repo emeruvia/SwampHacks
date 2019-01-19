6f62b407cbd95e34d40dc2cbeeec421380d9890e
Edgar Meruvia <emeruvia@gmail.com>
2019-01-19 16:15:35 -0500

    Refactor app

--- android-app/app/src/androidTest/java/com/swamphacks/ExampleInstrumentedTest.kt
+++ android-app/app/src/androidTest/java/com/swamphacks/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.swamphacks
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.myapplication", appContext.packageName)
+        assertEquals("com.swamphacks", appContext.packageName)
     }
 }
--- android-app/app/src/test/java/com/swamphacks/ExampleUnitTest.kt
+++ android-app/app/src/test/java/com/swamphacks/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.swamphacks
 
 import org.junit.Test
 
