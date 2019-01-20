467bb493a53484b74e91e13d0980ef0bfc468366
Edgar Meruvia <emeruvia@gmail.com>
2019-01-19 19:07:41 -0500

    Web text recognition is working

--- android-app/app/src/main/java/com/swamphacks/MainActivity.kt
+++ android-app/app/src/main/java/com/swamphacks/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.swamphacks
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +24,22 @@ class MainActivity : AppCompatActivity() {
   }
 
   fun recognizeBtn(view: View) {
-    Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT)
+        .show()
+    val bitmap = BitmapFactory.decodeResource(
+        resources,
+        R.drawable.receipt
+    )
+    val imageToBeAnalyzed = FirebaseVisionImage.fromBitmap(bitmap)
+      val detector = FirebaseVision.getInstance()
+          .onDeviceTextRecognizer
+
+      val result = detector.processImage(imageToBeAnalyzed)
+        .addOnSuccessListener { firebaseVisionText ->
+          println(firebaseVisionText.text)
+        }
+        .addOnFailureListener {
+          Log.d("failed", "trash")
+        }
   }
 }
