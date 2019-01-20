6905e88c7f1e77d11e5875257b3ef88cc79916b5
Edgar Meruvia <emeruvia@gmail.com>
2019-01-20 04:26:40 -0500

    Implement storage upload

--- android-app/app/src/main/java/com/swamphacks/MainActivity.kt
+++ android-app/app/src/main/java/com/swamphacks/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.swamphacks
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import com.google.firebase.ml.vision.FirebaseVision
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- android-app/app/src/main/java/com/swamphacks/PhotoActivity.kt
+++ android-app/app/src/main/java/com/swamphacks/PhotoActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -17,30 +18,36 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-import android.R.attr.pivotY
-import android.R.attr.pivotX
-import android.R.attr.angle
 import android.view.View
 import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class PhotoActivity : AppCompatActivity() {
 
   private val REQUEST_IMAGE_CAPTURE = 1
   lateinit var mCurrentPhotoPath: String
   lateinit var photoIv: ImageView
+  lateinit var lottieIv: LottieAnimationView
+  private var imageBitmap: Bitmap? = null
+  private var imageName: String = ""
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_photo)
     photoIv = findViewById(R.id.photo_iv)
+    lottieIv = findViewById(R.id.loading_wave)
     dispatchTakePictureIntent()
   }
 
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+  override fun onActivityResult(
+    requestCode: Int,
+    resultCode: Int,
+    data: Intent?
+  ) {
     if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
       setPic()
     }
@@ -73,18 +80,17 @@ class PhotoActivity : AppCompatActivity() {
     }
   }
 
-
   @Throws(IOException::class)
   private fun createImageFile(): File {
     // Create an image file name
     val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
     val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     val image = File.createTempFile(
-      "JPEG_$timeStamp", /* prefix */
-      ".jpg", /* suffix */
-      storageDir      /* directory */
+        "JPEG_$timeStamp", /* prefix */
+        ".jpg", /* suffix */
+        storageDir      /* directory */
     )
-
+    imageName = image.name
     // Save a file: path for use with ACTION_VIEW intents
     mCurrentPhotoPath = image.absolutePath
     Log.d("ImagePath", mCurrentPhotoPath)
@@ -120,9 +126,10 @@ class PhotoActivity : AppCompatActivity() {
           val rotatedBitmap = Bitmap.createBitmap(
               bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true
           )
-
+          imageBitmap = rotatedBitmap
           recognize(rotatedBitmap)
           photoIv.setImageBitmap(rotatedBitmap)
+          uploadToStorage()
         }
   }
 
@@ -140,6 +147,36 @@ class PhotoActivity : AppCompatActivity() {
         .addOnFailureListener {
           Log.d("failed", "trash")
         }
+  }
+
+  private fun uploadToStorage() {
+    if (imageBitmap == null) return
+    val storage = FirebaseStorage.getInstance()
+
+    // Create a storage reference from our app
+    val storageRef = storage.reference
+
+    // Create a reference to "mountains.jpg"
+    val imageRef = storageRef.child(imageName)
+
+    photoIv.isDrawingCacheEnabled = true
+    photoIv.buildDrawingCache()
+    val bitmap = (photoIv.drawable as BitmapDrawable).bitmap
+    val baos = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    val data = baos.toByteArray()
+
+    var uploadTask = imageRef.putBytes(data)
+    uploadTask.addOnFailureListener {
+      // Handle unsuccessful uploads
+      println("Failure")
+    }.addOnSuccessListener {
+      // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+      // ...
+      lottieIv.pauseAnimation()
+      lottieIv.visibility = View.GONE
+      println("Success")
+    }
   }
 
 }
