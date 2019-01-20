baeb55378000226cca6d8e6839196fad3b9fac05
Edgar Meruvia <emeruvia@gmail.com>
2019-01-20 00:51:05 -0500

    Update UI

--- android-app/app/src/main/java/com/swamphacks/MainActivity.kt
+++ android-app/app/src/main/java/com/swamphacks/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.swamphacks
 
-import android.content.Context
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
-import com.airbnb.lottie.LottieAnimationView
-import com.google.firebase.FirebaseApp
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 class MainActivity : AppCompatActivity() {
 
-  lateinit var image: ImageView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    image = findViewById(R.id.main_iv)
-    image.setImageResource(R.drawable.receipt2)
+  }
+
+  fun uploadButton(view: View) {
+    Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT)
+        .show()
   }
 
   fun recognizeBtn(view: View) {
